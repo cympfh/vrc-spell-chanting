aec4be6f69e3475a5dc6b3c9c88cf70b8c1f6344
cympfh <cympfh@gmail.com>
2023-04-14 18:08:23 +0900

    textchat function

--- README.md
+++ README.md
@@ -34,3 +34,32 @@ make serve-docker
 ```
 
 And open `localhost:8080`.
+
+## config.toml
+
+```toml
+[spell]
+lang       # Language for Chrome/SpeechRecognition
+threshold  # How rate you can allow for miss recognition
+commands   # An array of command, which has spell and OSC destination and arguments
+
+[vrchat]
+send_port  # 9000 by default
+recv_port  # 9000 by default
+host       # hostname or IP of VRChat. Usually "localhost" is OK.
+```
+
+### spell.commands
+
+A command has 3 fields.
+
+```toml
+{
+    spell,  # Your speech text
+    dest,   # OSC destination (e.g. /avatar/parameters/XXX)
+    args,   # Usually, one parameter (int, float, string)
+}
+```
+
+There are 2 special `dest`, `/textchat/start` and `/textchat/end`.
+Once `/textchat/start`, later your speech will be broadcasted as textchat, until `/textchat/end`.
--- server.py
+++ server.py
@@ -26,45 +26,80 @@ if config["vrchat"]["host"] == "$WIN":
 logger.info(config)
 
 
-class OSC:
-    def __init__(self, config):
-        host = config["vrchat"]["host"]
-        port = config["vrchat"]["send_port"]
-        logger.info("Connecting to host=%s, port=%s", host, port)
-        self.client = udp_client.SimpleUDPClient(host, port)
-
-    def send(self, dest: str, args: list):
-        logger.info("send: %s %s", dest, args)
-        self.client.send_message(dest, args)
-
-
-client = OSC(config)
-
-
 class Spell(BaseModel):
     text: str
 
 
-@app.post("/api/spell")
-async def post_spell(spell: Spell):
-    """Run spell"""
-    assert len(spell.text) > 0
-    logger.info("Running spell: %s", spell)
-    d, cmd = min(
+def similar_spell(spell: Spell) -> tuple[bool, float, dict]:
+    dist, cmd = min(
         [
             (distance(spell.text.lower(), cmd["spell"].lower()) / len(spell.text), cmd)
             for cmd in config["spell"]["commands"]
         ],
         key=lambda t: t[0],
     )
-    if d > config["spell"]["threshold"]:
-        logger.warn("Cannot found similar spells")
-        logger.warn("Most similar is: %s with d=%s", cmd, d)
-        return {"status": 404}
-    else:
-        logger.info("Similar spell found: %s with d=%s", cmd, d)
+    ok = dist <= config["spell"]["threshold"]
+    return ok, dist, cmd
+
+
+class OSC:
+
+    client: udp_client.SimpleUDPClient
+    chating: bool
+
+    def __init__(self, config):
+        host = config["vrchat"]["host"]
+        port = config["vrchat"]["send_port"]
+        logger.info("Connecting to host=%s, port=%s", host, port)
+        self.client = udp_client.SimpleUDPClient(host, port)
+        self.chating = False
+
+    def chat_mode(self, dest: str) -> bool:
+        if dest == "/textchat/start":
+            self.chating = True
+            return True
+        elif dest == "/textchat/end":
+            self.chating = False
+            return True
+        else:
+            return False
+
+    def send(self, dest: str, args: list):
+        self.client.send_message(dest, args)
+
+    def chat(self, message: str):
+        self.client.send_message("/chatbox/input", [message, True])
+
+
+client = OSC(config)
+
+
+@app.post("/api/spell")
+async def post_spell(spell: Spell):
+    """Run spell"""
+    logger.info("POST spell: %s", spell)
+    assert len(spell.text) > 0
+    ok, dist, cmd = similar_spell(spell)
+
+    if ok and client.chat_mode(cmd["dest"]):
+        if client.chating:
+            logger.info("Start to chat")
+            return {"status": 200, "spell": "[chat mode]"}
+        else:
+            logger.info("End to chat")
+            return {"status": 200, "spell": "[spell mode]"}
+    elif client.chating:
+        logger.info("Chat: %s", spell.text)
+        client.chat(spell.text)
+        return {"status": 200, "spell": "[chat mode]"}
+    elif ok:
+        logger.info("Running Spell: %s with dist=%s", cmd, dist)
         client.send(cmd["dest"], cmd["args"])
         return {"status": 200, "spell": cmd["spell"]}
+    else:
+        logger.warn("Cannot found similar spells")
+        logger.warn("Most similar one is: %s with dist=%s", cmd, dist)
+        return {"status": 404}
 
 
 @app.get("/api/lang")
