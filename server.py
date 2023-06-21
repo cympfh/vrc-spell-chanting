e927e6feaac96b68ed64407b2cab82361eb23ed1
cympfh <cympfh@gmail.com>
2023-06-21 21:00:14 +0900

    fix

--- server.py
+++ server.py
@@ -89,7 +89,7 @@ class OSC:
             translate_result.append(t)
         m = " / ".join(translate_result)
         logger.info(f"Chat with translate: {m}")
-        self.client.send_message("/chatbox/input", [m, True])
+        self.client.send_message("/chatbox/input", [m, True, True])
 
 
 client = OSC(config)
